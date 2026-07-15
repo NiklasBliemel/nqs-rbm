082aa13121b99a626e731009c47645496e8cbcb5
Niklas Bliemel <niklas.bliemel@gmail.com>
2026-07-15 11:14:24 +0200

    add description

--- hg_flax_helperfunctions.py
+++ hg_flax_helperfunctions.py
@@ -11,8 +11,12 @@ def hf_upload_flax_parameter(parameter: dict, repo_id: str, branch_name: str = "
             file.write(packed)
         upload_file(path_or_fileobj=os_path, path_in_repo="parameter.msgpack", repo_id=repo_id, revision=branch_name, commit_message="uploade parameter")
 
-def hf_download_flax_parameter(repo_id: str, branch_name: str = "main") -> dict:
-    path = hf_hub_download(repo_id=repo_id, revision=branch_name, filename="parameter.msgpack")
+def hf_download_flax_parameter(repo_id: str, model_branch: str = "main") -> dict:
+    '''''
+    Example:
+    parameter = hf_download_flax_parameter("NiklasBli/nqs-rbm", "rbm_10_6_0.7")
+    '''''
+    path = hf_hub_download(repo_id=repo_id, revision=model_branch, filename="parameter.msgpack")
     with open(path, 'rb') as file:
         data = file.read()
         return serialization.msgpack_restore(data)
